6c2d42ec8d8b5791eead36aaf1202bf4a3961b9a
David Rio Deiros <driodeiros@gmail.com>
2014-01-15 15:49:36 -0500

    nc

--- example/depth/build.sh
+++ example/depth/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-n_samples=10
+n_samples=25
 chrm=Chr17
 
 ./gen_html.py $chrm $n_samples > index.html
@@ -9,12 +9,15 @@ for i in `seq 1 $n_samples`
 do
     out="sample${i}.bed"
     echo $out
-    ./gen_bed.py $chrm 500 1000 > $out
-    ./gen_bed.py $chrm 1001 3000 0 >> $out
-    ./gen_bed.py $chrm 3001 4000 >> $out
-    ./gen_bed.py $chrm 4001 7000 20 >> $out
-    ./gen_bed.py $chrm 7001 20000 >> $out
+    (
+    ./gen_bed.py $chrm 500 1000 > $out ;
+    ./gen_bed.py $chrm 1001 3000 0 >> $out ;
+    ./gen_bed.py $chrm 3001 4000 >> $out ;
+    ./gen_bed.py $chrm 4001 7000 20 >> $out ;
+    ./gen_bed.py $chrm 7001 20000 >> $out ;
+    ) &
 done
+wait
 
 
 
