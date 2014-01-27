870e3e6b5765645573b765f9cb73d5c9bbbf89fa
David Rio Deiros <driodeiros@gmail.com>
2014-01-27 15:18:25 -0500

    Final touches for the example with real data (read depth).

--- example/simulated/gen_html.py
+++ example/simulated/gen_html.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 
 import sys
-import glob
 from jinja2 import Template
 
-start = 1100000
-stop = 1200000
+start = 500
+stop = 20000
 size = 1280
-chrm = "Chr17"
+chrm = sys.argv[1]
 step = 2
 
+n_samples = int(sys.argv[2])
+seed = "sample"
 samples = []
-for b in glob.glob("*.bed"):
-    samples.append(b)
+for i in range(1, n_samples+1):
+    f_name = seed + str(i) + ".bed"
+    samples.append(f_name)
 
 template = Template(open("index.template.html").read())
 print template.render(start=start, stop=stop, size=size, chrm=chrm, step=step, metrics=samples)
