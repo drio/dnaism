f12874b53fd73b87dacfbdc6ca5b826f5aa37087
David Rio Deiros <driodeiros@gmail.com>
2014-03-18 10:11:35 -0700

    Fixing logic in bedserver. Now properly pulls data from server.

--- src/bedserver.js
+++ src/bedserver.js
@@ -4,15 +4,17 @@ dnaism_contextPrototype.bedserver = function(host) {
   var source = {},
       context = this;
 
-  source.metric = function(sample, project) {
-    return context.metric(function(start, stop, step, callback) {
-      d3.json(host + "/bedserver/api/v1.0/samples/"
-          + project + "/" +
+  source.metric = function(project, sample) {
+    return context.metric(function(start, stop, chrm, step, callback) {
+      var url = host + "/bedserver/api/v1.0/samples/"
+          + project + "/" + sample
           + "?start=" + start
           + "&stop=" + stop
-          + "&chrm=" + chrm
-          + "&size=" + context.size
-          + "&step=" + step, function(data) {
+          + "&chrm=" + context.chrm()
+          + "&size=" + context.size()
+          + "&step=" + step;
+      console.log("bedserver; url= " + url);
+      d3.json(url, function(data) {
         if (!data) return callback(new Error("unable to load data"));
         callback(null, data);
       });
